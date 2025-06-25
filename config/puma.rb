bf7d1c3845a875ff13973bf6354af8e77286c352
Tomohiro Mitani <mitani.work@gmail.com>
2025-06-25 21:40:40 +0900

    Initial commit

---
+++ config/puma.rb
@@ -0,0 +1,12 @@
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
+threads threads_count, threads_count
+
+preload_app!
+
+# Support IPv6 by binding to host `::` instead of `0.0.0.0`
+port(ENV.fetch("PORT") { 3000 }, "::")
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
